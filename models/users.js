d8bddea11b3957638c995e1be35920815f47cf5e
Justin8606 <justinshajuk@gmail.com>
2025-02-06 17:15:28 +0530

    signUp api completed

---
+++ models/users.js
@@ -0,0 +1,23 @@
+const Mongoose = require("mongoose")
+
+const userSchema = Mongoose.Schema(
+    {
+        name:{
+            type:String,
+            required:true
+        },
+        phone:String,
+        email:{
+            type:String,
+            required:true
+        },
+        password:{
+            type:String,
+            required:true
+        }
+    }
+)
+//after schema creation convert it into model
+
+var userModel = Mongoose.model("users",userSchema)
+module.exports = userModel
